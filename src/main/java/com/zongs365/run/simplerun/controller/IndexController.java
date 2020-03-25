227db2694d60b51dc54dd5939cd2fdb4d66cbcb0
tengdongjie <tengdongjie@zongs365.com>
2020-03-25 15:11:24 +0800

    删除http方式执行

--- src/main/java/com/zongs365/run/simplerun/controller/IndexController.java
+++ src/main/java/com/zongs365/run/simplerun/controller/IndexController.java
@@ -3,12 +3,7 @@ package com.zongs365.run.simplerun.controller;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-
-import java.io.InputStreamReader;
-import java.io.LineNumberReader;
 
 /**
  * @author tengdj
@@ -19,37 +14,9 @@ import java.io.LineNumberReader;
 @RequestMapping("/")
 public class IndexController {
 
-    @GetMapping({"/", "/index"})
-    public String index() {
-        return "restart";
-    }
-
-    @GetMapping("websocket")
+    @GetMapping({"/", "/index", "websocket"})
     public String wb() {
         return "restart-wb";
-    }
-
-    @ResponseBody
-    @RequestMapping("/restart/{name}")
-    public String restart(@PathVariable("name") String name) {
-        String cmd = "/deploy/" + name + "/start-api.sh";
-        StringBuilder stringBuilder = new StringBuilder();
-        try {
-            Process process = Runtime.getRuntime().exec(new String[]{"/bin/sh", "-c", cmd}, null, null);
-            InputStreamReader ir = new InputStreamReader(process.getInputStream());
-            LineNumberReader input = new LineNumberReader(ir);
-            String line;
-            process.waitFor();
-            while ((line = input.readLine()) != null) {
-                log.info(line);
-                stringBuilder.append(line).append("\r\n");
-            }
-        } catch (Exception e) {
-            for (StackTraceElement ee : e.getStackTrace()){
-                stringBuilder.append(ee.toString()).append("\r\n");
-            }
-        }
-        return stringBuilder.toString();
     }
 
 }
